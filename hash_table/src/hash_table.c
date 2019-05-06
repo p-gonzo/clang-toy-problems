d710b91e31ba9fe3549d4a92b56d35a167436212
Philip Gonzalez <pgonzalez115@gmail.com>
2019-05-06 12:29:56 -0600

    Initial functional implementation of a C hash table

---
+++ hash_table/src/hash_table.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <string.h>
+
+#include "hash_table.h"
+
+int HT_PRIME_1 = 151;
+int HT_PRIME_2 = 163;
+
+static ht_item* ht_new_item(const char* k, const char* v) {
+    ht_item* i = malloc(sizeof(ht_item));
+    i->key = strdup(k);
+    i->value = strdup(v);
+    return i;
+}
+
+ht_hash_table* ht_new() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+    ht->size = 53;
+    ht->count = 0;
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item*));
+    return ht;
+}
+
+static int ht_hash(const char* s, const int a, const int m) {
+    long hash = 0;
+    const int len_s = strlen(s);
+    for (int i = 0; i < len_s; i++) {
+        hash += (long)pow(a, len_s - (i + 1)) * s[i];
+        hash = hash % m;
+    }
+    return (int)hash;
+}
+
+static int ht_get_hash( const char* s, const int num_buckets, const int attempt) {
+     const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+     const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+ }
+
+static void ht_del_item(ht_item* i) {
+    free(i->key);
+    free(i->value);
+    free(i);
+}
+
+void ht_del_hash_table(ht_hash_table* ht){
+    for (int i = 0; i < ht->size; i++) {
+        ht_item* item = ht->items[i];
+        if (item != NULL) {
+            ht_del_item(item);
+        }
+    }
+    free(ht-> items);
+    free(ht);
+}
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
+    ht_item* new_item = ht_new_item(key, value);
+    int target_index = ht_get_hash(new_item->key, ht->size, 0);
+    ht_item* target_bucket = ht->items[target_index];
+    int i = 1;
+    while (target_bucket != NULL) {
+        target_index = ht_get_hash(new_item->key, ht->size, i);
+        target_bucket = ht->items[target_index];
+        i++;
+    }
+    ht->items[target_index] = new_item;
+    ht->count++;
+}
+
+char* ht_search(ht_hash_table* ht, const char* key) {
+    int target_index = ht_get_hash(key, ht->size, 0);
+    ht_item* target_item = ht->items[target_index];
+    int i = 1;
+    while (target_item != NULL) {
+        if (strcmp(target_item->key, key)== 0) {
+            return target_item->value;
+        }
+        target_index = ht_get_hash(key, ht->size, i);
+        target_item = ht->items[target_index];
+        i++;
+    }
+    return NULL;
+}
+
+int main() {
+    ht_hash_table* ht = ht_new();
+    ht_insert(ht, "Hello", "World");
+    puts(ht_search(ht, "Hello"));
+    puts(ht_search(ht, "Nope"));
+    ht_del_hash_table(ht);
+    return 0;
+}
+
